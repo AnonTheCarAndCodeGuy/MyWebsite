88991f816b8fe2b80ac71a8c0f23748259e1423d
AnonTheCarAndCodeGuy <fadhilah.a.n75@gmail.com>
2015-10-03 16:16:13 +0700

    refactor project shop

--- MyWebsiteProject/settings.py
+++ MyWebsiteProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Portofolio',
+    'catalog',
 )
 
 MIDDLEWARE_CLASSES = (
--- MyWebsiteProject/urls.py
+++ MyWebsiteProject/urls.py
@@ -1,9 +1,24 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from catalog import views
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'Portofolio.views.index', name='index'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^cartrader/', views.HomeView.as_view(), name="home"),
+    url(r'^login/', views.LoginView.as_view(), name="login"),
+    url(r'^carlot/', views.CarlotView.as_view(), name="carlot"),
+    url(r'^about/', views.AboutView.as_view(), name="about"),
+    url(r'^faq/', views.FaqView.as_view(), name="faq"),
+    url(r'^dashboard/', views.DashboardView.as_view(), name="dashboard"),
+    url(r'^register/', views.RegisterView.as_view(), name="register"),
+    url(r'^profile/', views.ProfileView.as_view(), name="profile"),
+    url(r'^logout/', views.LogoutView.as_view(), name="logout"),
+    url(r'^addcar/', views.AddcarView.as_view(), name="addcar"),
+    url(r'^cardetail/(?P<car_id>\w+)/detail',views.CardetailView.as_view(car_id=None), name='cardetail'),
+    url(r'^addbrand/', views.AddbrandView.as_view(), name="addbrand"),
+    url(r'^mycars/(?P<owner_id>\w+)/all', views.MycarsView.as_view(owner_id=None), name="mycars"),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
